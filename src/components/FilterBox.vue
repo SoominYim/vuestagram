f227dcaf360b58439bcea0b5a2bcc878a9e4a020
Soomin Yim <soo15soo@kakao.com>
2022-07-07 22:13:00 +0900

    first commit

---
+++ src/components/FilterBox.vue
@@ -0,0 +1,38 @@
+<template>
+    <div
+        class="filter-item"
+        @click="send"
+        :style="`background-image:url(${uploadImage})`"
+    >
+        <!-- 슬롯에 name을 안주면 부모에서 맘대로 써도된다 -->
+        <slot name="filterName"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FilterBox",
+    methods: {
+        send() {
+            this.emitter.emit("send", this.filter); //('작명','데이터')
+        },
+    },
+    props: {
+        uploadImage: String,
+        filter: String,
+    },
+};
+</script>
+
+<style>
+.filter-item {
+    width: 100px;
+    height: 100px;
+    margin: 10px 10px 10px auto;
+    padding: 8px;
+    display: inline-block;
+    color: white;
+    background-size: cover;
+    background: #333;
+}
+</style>
---
+++ src/components/GramContainer.vue
@@ -0,0 +1,135 @@
+<template>
+    <div>
+        <div v-if="step == 0">
+            <gram-post :gramData="data" v-for="(data, i) in gramData" :key="i">
+            </gram-post>
+        </div>
+
+        <!-- 필터선택페이지 -->
+        <div v-if="step == 1">
+            <div
+                class="upload-image"
+                :style="`background-image: url(${uploadImage})`"
+                :class="imageFilter"
+            ></div>
+            <div class="filters">
+                <filter-box
+                    :uploadImage="uploadImage"
+                    v-for="(filter, i) in filters"
+                    :key="i"
+                    :class="filter"
+                    :filter="filter"
+                >
+                    <!-- slot에 이름 안주면 바로바로 쓸수있다 -->
+                    <template v-slot:filterName>{{ filter }}</template>
+                </filter-box>
+            </div>
+        </div>
+
+        <!-- 글작성페이지 -->
+        <div v-if="step == 2">
+            <div
+                class="upload-image"
+                :style="`background-image: url(${uploadImage})`"
+                :class="imageFilter"
+            ></div>
+            <div class="write">
+                <textarea
+                    @input="$emit('write', $event.target.value)"
+                    class="write-box"
+                ></textarea>
+            </div>
+        </div>
+        <div v-if="step == 3">
+            <my-page></my-page>
+        </div>
+    </div>
+</template>
+
+<script>
+import GramPost from "@/components/GramPost.vue";
+import FilterBox from "@/components/FilterBox";
+import MyPage from "@/components/MyPage";
+
+export default {
+    name: "GramContainer",
+    components: {
+        GramPost,
+        FilterBox,
+        MyPage,
+    },
+    props: {
+        gramData: Array,
+        step: Number,
+        uploadImage: String,
+        imageFilter: String,
+    },
+    data() {
+        return {
+            filters: [
+                "aden",
+                "_1977",
+                "brannan",
+                "brooklyn",
+                "clarendon",
+                "earlybird",
+                "gingham",
+                "hudson",
+                "inkwell",
+                "kelvin",
+                "lark",
+                "lofi",
+                "maven",
+                "mayfair",
+                "moon",
+                "nashville",
+                "perpetua",
+                "reyes",
+                "rise",
+                "slumber",
+                "stinson",
+                "toaster",
+                "valencia",
+                "walden",
+                "willow",
+                "xpro2",
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+.upload-image {
+    width: 100%;
+    height: 450px;
+    background-size: cover;
+}
+.filters {
+    overflow-x: scroll;
+    white-space: nowrap;
+}
+
+.filters::-webkit-scrollbar {
+    height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+    background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+.write-box {
+    border: none;
+    width: 90%;
+    height: 100px;
+    padding: 15px;
+    margin: auto;
+    display: block;
+    outline: none;
+}
+</style>
