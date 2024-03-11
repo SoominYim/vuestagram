886b596937f0102a8f16b3fab5955a80f3d5013e
SoominYim <soo15soo@kakao.com>
2024-03-11 16:22:38 +0900

    모바일 확대 방지

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,32 @@ export default {
       login: this.$store.state.login,
     };
   },
+  mounted() {
+    // 두 손가락으로 화면을 클릭 시 이벤트 무시
+    document.documentElement.addEventListener(
+      "touchstart",
+      function (event) {
+        if (event.touches.length > 1) {
+          event.preventDefault();
+        }
+      },
+      false
+    );
+
+    // 두번 연속 탭이 0.3초보다 짧을 경우 이벤트 무시
+    var lastTouchEnd = 0;
+    document.documentElement.addEventListener(
+      "touchend",
+      function (event) {
+        var now = new Date().getTime();
+        if (now - lastTouchEnd <= 300) {
+          event.preventDefault();
+        }
+        lastTouchEnd = now;
+      },
+      false
+    );
+  },
 };
 </script>
 
